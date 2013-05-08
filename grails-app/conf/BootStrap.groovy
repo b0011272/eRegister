ee1e7ec56b63a892ab1114776015791196672d6f
student <student@WebArch.localdomain>
2013-05-08 21:09:35 +0100

    create bootstrap sql to load when grails is run

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,23 @@
+import uk.ac.shu.webarch.eregister.*;
+
 class BootStrap {
 
     def init = { servletContext ->
+
+println("BootStrap::init");
+
+def ian_instructor = Instructor.findByStaffId('15755675') ?: new Instructor(staffId:'15755675', 
+									    name:'Ian Ibbotson').save();
+
+def web_arch_course = Course.findByCourseCode('7458536') ?: new Course(courseCode:'7458536',
+								       courseName:'Web Architectures',
+								       description:'Brilliant lectures').save();
+
+def ste_student = Student.findByStudentNumber('1407885') ?: new Student(studentNumber:'1407885',
+								        fullStudentName:'Stephen Clark',
+								        notes:'Top Student').save();
+								     
+
     }
     def destroy = {
     }
